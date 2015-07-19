b23649dc825ac9791936ec3fc5ac9c286067a1cf
Marcus Chang <marcus.chang@arm.com>
2015-07-19 12:49:51 +0100

    Fixed syntax error in spi_api.c when compiling in debug mode.

--- source/spi_api.c
+++ source/spi_api.c
@@ -35,7 +35,7 @@
 static uint16_t fill_word = SPI_FILL_WORD;
 #define SPI_LEAST_ACTIVE_SLEEPMODE EM1
 
-inline CMU_Clock_TypeDef spi_get_clock_tree(spi_t *obj)
+static inline CMU_Clock_TypeDef spi_get_clock_tree(spi_t *obj)
 {
     switch ((int)obj->spi.spi) {
 #ifdef USART0
